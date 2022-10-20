264c20c81e0ef0106e4ec1580aa8f4205f754158
Julio Facal <starlyn.dev@gmail.com>
2022-10-19 23:18:36 -0300

    movimentando objetos na tela

--- pacman.py
+++ pacman.py
@@ -1,21 +1,43 @@
 import pygame
 
 AMARELO = (255, 255, 0)
+VELOCIDADE = 0.1
 
 
 pygame.init()
 
 tela = pygame.display.set_mode((640, 480))
 
+xpos = 30
+ypos = 240
+
+vel_x = VELOCIDADE
+vel_y = VELOCIDADE
+
+raio = 30
+
 while True:
     
-    pygame.draw.circle(tela, AMARELO, (320, 240), 50, 0)
+    xpos += vel_x
+    
+    ypos += vel_y
+    
+    if xpos > 640 - raio:
+        vel_x = -VELOCIDADE
+    elif xpos < 0 + raio:
+        vel_x = VELOCIDADE
+        
+    if ypos > 480 - raio:
+        vel_y = -VELOCIDADE
+    elif ypos < 0 + raio:
+        vel_y = VELOCIDADE
+    
+    tela.fill((0, 0, 0))
+    pygame.draw.circle(tela, AMARELO, (int(xpos), ypos), raio, 0)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
-    tela.fill((0, 0, 0))
 
     pygame.display.update()
